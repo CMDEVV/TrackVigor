7d8ce72870867f335e17d615c4bd6e80ed8fd77e
CarlosMendezzz <carlos.mendez@25-2.com>
2023-10-07 12:07:26 -0600

    Initial Commit

---
+++ TrackVigor/TrackVigorApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrackVigorApp.swift
+//  TrackVigor
+//
+//  Created by Carlos Mendez on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackVigorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
